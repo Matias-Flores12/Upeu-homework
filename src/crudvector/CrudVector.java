4e4885319c2b9e555908756057962ae26f0e1da3
Matias-Flores12 <matiasfloresaguilar12@gmail.com>
2021-10-08 21:32:15 -0500

    Examen-POO_1

---
+++ src/crudvector/CrudVector.java
@@ -0,0 +1,11 @@
+package crudvector;
+
+public class CrudVector {
+
+    
+    public static void main(String[] args) {
+           CrudVectorFrame cFrame = new CrudVectorFrame();
+           cFrame.show();
+    }
+    
+}
---
+++ src/crudvector/datosPersona.java
@@ -0,0 +1,42 @@
+package crudvector;
+
+public class datosPersona {
+    
+    public String nombre;
+    public String codigo;
+    public String ap_paterno;
+    public String ap_materno;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getAp_paterno() {
+        return ap_paterno;
+    }
+
+    public void setAp_paterno(String ap_paterno) {
+        this.ap_paterno = ap_paterno;
+    }
+
+    public String getAp_materno() {
+        return ap_materno;
+    }
+
+    public void setAp_materno(String ap_materno) {
+        this.ap_materno = ap_materno;
+    }
+    
+}
